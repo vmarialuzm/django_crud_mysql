c5c6e4ee169ecec5415d06ee45d0a21c414150ba
vmarialuz <vmarialuzm@gmail.com>
2023-10-05 16:57:46 -0500

    vista del CRUD de libros

--- libreria/views.py
+++ libreria/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def inicio(request):
-    return HttpResponse("Bienvenido Develoteca")
+    return HttpResponse("Hola Rafa")
 
 def nosotros(request):
     return render(request, 'paginas/nosotros.html')
