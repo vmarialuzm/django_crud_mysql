d992f03a184f4c44635d71dcfaf41325880a0cb9
vmarialuz <vmarialuzm@gmail.com>
2023-10-05 19:40:58 -0500

    incluir formulario en vista

--- libreria/urls.py
+++ libreria/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('', views.inicio, name='inicio'),
     path('nosotros/', views.nosotros, name='nosotros'),
     path('libros/', views.libros, name='libros'),
+    path('libros/crear/', views.crear, name='crear'),
+    path('libros/editar/', views.editar, name='editar'),
 ]
--- libreria/views.py
+++ libreria/views.py
@@ -2,10 +2,16 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def inicio(request):
-    return HttpResponse("Hola Rafa")
+    return render(request, 'paginas/inicio.html')
 
 def nosotros(request):
     return render(request, 'paginas/nosotros.html')
 
 def libros(request):
     return render(request, 'libros/index.html')
+
+def crear(request):
+    return render(request, 'libros/crear.html')
+
+def editar(request):
+    return render(request, 'libros/editar.html')
