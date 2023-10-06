c5d446d1cba3756ca44bbb69b1838b87adcb21b9
vmarialuz <vmarialuzm@gmail.com>
2023-10-06 10:46:33 -0500

    borrando registro

--- libreria/urls.py
+++ libreria/urls.py
@@ -9,5 +9,6 @@ urlpatterns = [
     path('libros/', views.libros, name='libros'),
     path('libros/crear/', views.crear, name='crear'),
     path('libros/editar/', views.editar, name='editar'),
+    path('eliminar/<int:id>', views.eliminar, name='eliminar'),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- libreria/views.py
+++ libreria/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Libro
 from .forms import LibroForm
@@ -14,8 +14,17 @@ def libros(request):
     return render(request, 'libros/index.html', {'libros': libros})
 
 def crear(request):
-    formulario = LibroForm(request.POST or None)
+    formulario = LibroForm(request.POST or None, request.FILES or None)
+    if formulario.is_valid():
+        formulario.save()
+        return redirect('libros')
     return render(request, 'libros/crear.html', {'formulario': formulario})
 
 def editar(request):
     return render(request, 'libros/editar.html')
+
+def eliminar(request, id):
+    libro = Libro.objects.get(id=id)
+    libro.delete()
+    return redirect('libros')
+
