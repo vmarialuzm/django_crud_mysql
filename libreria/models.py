6e49fa9726b06b4429785403c4e5b2dc319d53a5
vmarialuz <vmarialuzm@gmail.com>
2023-10-05 20:02:51 -0500

    modelo y migraciones

--- libreria/models.py
+++ libreria/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class Libro(models.Model):
+    titulo = models.CharField(max_length=100, verbose_name='Título')
+    imagen = models.ImageField(upload_to='imagenes/', verbose_name='Imagen', null=True)
+    description = models.TextField(verbose_name='Descripción', null=True)
+
